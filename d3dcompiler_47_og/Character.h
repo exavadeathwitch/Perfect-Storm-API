12f08bb8f8096636982a76c0b224053e2371a407
exavadeathwitch <68618456+exavadeathwitch@users.noreply.github.com>
2020-12-09 16:42:08 -0500

    Fixing Character Functions

--- d3dcompiler_47_og/Character.h
+++ d3dcompiler_47_og/Character.h
@@ -2,12 +2,16 @@
 
 #include <stdio.h>
 #include "Vector3.h"
+inline bool P1isguarding = 0;
+inline bool P1cantilt = 0;
+inline bool P2isguarding = 0;
+inline bool P2cantilt = 0;
 class Character
 {
 	public:
-	static void Character::Initialize(uintptr_t p, uintptr_t s);
-	static void Character::SetParam1cmn(uintptr_t p, uintptr_t s, int characode);
-	static void Character::SetParam7ssx(uintptr_t p, uintptr_t s, int characode);
-	static void Character::SetParam7brt(uintptr_t p, uintptr_t s, int characode);
+	static void Character::Initialize(uintptr_t p, uintptr_t s, int P);
+	static void Character::SetParam1cmn(uintptr_t p, uintptr_t s, int P);
+	static void Character::SetParam7ssx(uintptr_t p, uintptr_t s, int P);
+	static void Character::SetParam7brt(uintptr_t p, uintptr_t s, int P);
 };
 
