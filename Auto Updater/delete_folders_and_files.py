e2e2e68d203421607a9eca297ffca0e9a2167847
absol <absol@DESKTOP-5780KD9>
2021-08-24 23:55:04 -0400

    Auto-Updater 100%

--- Auto Updater/delete_folders_and_files.py
+++ Auto Updater/delete_folders_and_files.py
@@ -3,36 +3,6 @@ import sys
 import shutil
 
 
-def delete() -> None:
-    """
-    This function checks if the 7 and 12 folder are in data/patch,
-    If they are, it deletes the 7 folder and renames the 12 folder to 7.
-    If only the 12 folder is there, nothing is deleted but the folder is renamed
-    and if there is only a 7 folder, nothing is also done.
-    """
-
-    # Get the current directory.
-    if getattr(sys, 'frozen', False):
-        application_path = os.path.dirname(sys.executable)
-    elif __file__:
-        application_path = os.path.dirname(__file__)
-
-    # Create the path for deleting.
-    delete_path = os.path.join(application_path, 'data', 'patch', '7')
-
-    # Create the path for renaming.
-    rename_path = os.path.join(application_path, 'data', 'patch', '12')
-
-    # Delete the 7 folder and rename the 12 to 7 only if both paths exist.
-    if os.path.exists(delete_path) and os.path.exists(rename_path):
-        shutil.rmtree(delete_path)
-        if os.path.exists(rename_path):
-            os.rename(rename_path, delete_path)
-
-    # Rename the 12 folder to 7.
-    if os.path.exists(rename_path):
-        os.rename(rename_path, delete_path)
-
 
 def delete_7z_file() -> None:
     """
--- Auto Updater/main.py
+++ Auto Updater/main.py
@@ -3,14 +3,14 @@ from go_daddy_dl import download_from_godaddy
 from zip_extraction import extract_files
 from launch_exe import start_storm4_exe
 import time
-from delete_folders_and_files import delete, delete_7z_file
+from delete_folders_and_files import delete_7z_file
 import requests
 
-pastebin_url = 'https://pastebin.com/raw/48DtyP9p'
+pastebin_url = 'https://pastebin.com/raw/zKsf7cVD'
 r = requests.get(pastebin_url)  # Check if the server or webpage exists.
 content = r.text  # Get the actual contents in the pastebin.
 
-delete()  # Call the delete function.
+ # Call the delete function.
 
 try:
     url = content
@@ -27,49 +27,9 @@ try:
     try:
         start_storm4_exe()  # Start the storm 4 exe.
     except FileNotFoundError as e:
-        print('I could not start your STORM 4 exe. Please try launching it manually')
+        print('The program could not start the game. Please try launching it manually')
         time.sleep(10)
 except Exception as e:
-    time.sleep(3)
-    try:
-        print('\n The Link included in the program does not work.')
-        url = input(' Please enter a media fire link that downloads the mod files \n :')
-        output = 'PerfectStorm.7z'
-        mediafire_dl.download(url, output, quiet=False)
-        print('\nAttempting to extract the files')
-        time.sleep(2)
-        extract_files(output)
-        print('\nFiles have been successfully extracted')
-        time.sleep(1)
-        print('\nDeleting the PerfectStorm.7z file.')
-        delete_7z_file()  # Call the delete function
-        input('\nAll operations have been completed. Please press enter to quit')
-        try:
-            start_storm4_exe()  # Start the storm 4 exe.
-        except FileNotFoundError as e:
-            print('I could not start your STORM 4 exe. Please try launching it manually')
-            time.sleep(10)
-    except Exception as e:
-        time.sleep(3)
-        try:
-            print('\n The link given does not work, attempting to download from the slower server. \n')
-            url = '  http://perfectstormmod.com/update/PerfectStorm.7z'
-            output = 'PerfectStorm.7z'
-            download_from_godaddy(url)  # Change this to the go daddy program
-            print('\nAttempting to extract the files')
-            time.sleep(2)
-            extract_files(output)
-            print('\nFiles have been successfully extracted')
-            time.sleep(1)
-            print('\nDeleting the PerfectStorm.7z file.')
-            delete_7z_file()  # Call the delete function
-            input('\nAll operations have been completed. Please press enter to quit')
-            try:
-                start_storm4_exe()  # Start the storm 4 exe.
-            except FileNotFoundError as e:
-                print('I could not start your STORM 4 exe. Please try launching it manually')
-                time.sleep(10)
-        except Exception as e:
             time.sleep(3)
-            print('\nThe program encountered an issue. Please contact the Administrators of the perfect Storm Mod to'
-                  ' Resolve this. Thank you.')
+            print('\nThe program could not download the files from the server. Please download them manually at: ')
+            print(url)
