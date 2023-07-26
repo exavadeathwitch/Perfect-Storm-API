77d06e7b8f558768018f3b59558ffa74dd88f664
exavadeathwitch <absolutewizardz@gmail.com>
2023-07-26 16:33:44 -0700

    Language Support Added

--- Dll/Src/Core/Settings/Settings.hpp
+++ Dll/Src/Core/Settings/Settings.hpp
@@ -3,6 +3,8 @@
 namespace settings {
 	struct LoadedSettings {
 		bool m_ShouldEnableConsole{};
+		bool m_ConvertMenuMessages{};
+		std::string m_Language{};
 		int m_MaxModsPerColumn{};
 		std::string m_Version{};
 		bool m_ShouldAutoUpdate{};
@@ -12,4 +14,5 @@ namespace settings {
 	void applyDefaults() noexcept;
 	void save() noexcept;
 	void load() noexcept;
+	void updateSettings();
 }
--- Dll/Src/Util/Util.hpp
+++ Dll/Src/Util/Util.hpp
@@ -10,4 +10,8 @@ namespace util {
 	static std::filesystem::path getSettingsPath() noexcept {
 		return getModPath() / "settings.json";
 	}
+
+	static std::filesystem::path getImguiStylePath() noexcept {
+		return getModPath() / "imguistyle.ini";
+	}
 }
